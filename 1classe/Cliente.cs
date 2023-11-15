fe3b86270c6ff1eb70b91c65ce3f5d6d7b0496d0
Nuno <nuninhon0n0@gmail.com>
2023-11-15 13:38:18 +0000

    Documentação da classe cliente feita

--- 1classe/Cliente.cs
+++ 1classe/Cliente.cs
@@ -7,16 +7,20 @@ using _2classe;
 
 namespace _1classe
 {
+    /// <summary>
+    /// Classe que representa um cliente.
+    /// </summary>
     public class Cliente
     {
+        /// <summary>
+        /// Estado da classe Cliente
+        /// </summary>
         #region Estado
-        private int idCliente;
-        private string nomeCliente;
-        private int nifCliente;
-        private string moradaCliente;
-        private int telemovelCliente;
-
-
+        private int idCliente;          // ID do cliente
+        private string nomeCliente;    // Nome do cliente
+        private int nifCliente;        // NIF (Número de Identificação Fiscal) do cliente
+        private string moradaCliente;  // Morada do cliente
+        private int telemovelCliente;  // Número de telemóvel do cliente
         #endregion
 
         #region Comportamentos
@@ -35,6 +39,15 @@ namespace _1classe
             telemovelCliente = 0;
         }
 
+        /// <summary>
+        /// Construtor com parâmetros para inicializar um Cliente com valores específicos.
+        /// </summary>
+        /// <param name="id">ID do cliente</param>
+        /// <param name="nome">Nome do cliente</param>
+        /// <param name="nif">NIF do cliente</param>
+        /// <param name="morada">Morada do cliente</param>
+        /// <param name="tel">Número de telemóvel do cliente</param>
+
         public Cliente(int id, string nome, int nif, string morada, int tel)
         {
             this.idCliente = id;
@@ -45,8 +58,12 @@ namespace _1classe
         }
         #endregion
 
+        // Propriedades para acessar os atributos privados da classe Cliente
         #region Propriedades
 
+        /// <summary>
+        /// Propriedade para acessar e modificar o ID do cliente.
+        /// </summary>
         public int IdCliente
         {
             set
@@ -60,6 +77,9 @@ namespace _1classe
             }
         }
 
+        /// <summary>
+        /// Propriedade para acessar e modificar o Nome do cliente.
+        /// </summary>
         public string NomeCliente
         {
             get
@@ -72,6 +92,9 @@ namespace _1classe
             }
         }
 
+        /// <summary>
+        /// Propriedade para acessar e modificar o NIF do cliente.
+        /// </summary>
         public int NifCliente
         {
             set
@@ -85,6 +108,9 @@ namespace _1classe
             }
         }
 
+        /// <summary>
+        /// Propriedade para acessar e modificar a morada do cliente.
+        /// </summary>
         public string MoradaCliente
         {
             get
@@ -97,6 +123,9 @@ namespace _1classe
             }
         }
 
+        /// <summary>
+        /// Propriedade para acessar e modificar o número de telmovel do cliente.
+        /// </summary>
         public int TelemovelCliente
         {
             set
@@ -113,6 +142,9 @@ namespace _1classe
 
         #region Operadores
 
+        /// <summary>
+        /// Função que verifica se dois clientes são iguais com base nos seus atributos.
+        /// </summary>
         public static bool operator ==(Cliente p1, Cliente p2)
         {
             if ((p1.idCliente == p2.idCliente) && (p1.nomeCliente == p2.nomeCliente) && (p1.nifCliente == p2.nifCliente) &&
@@ -121,6 +153,9 @@ namespace _1classe
             return false;
         }
 
+        /// <summary>
+        /// Função que verifica se dois clientes são diferentes com base nos seus atributos.
+        /// </summary>
         public static bool operator !=(Cliente p1, Cliente p2)
         {
             if (p1 == p2)
@@ -131,11 +166,17 @@ namespace _1classe
 
         #region Overrides
 
+        /// <summary>
+        /// Função que retorna uma representação textual do cliente.
+        /// </summary>
         public override string ToString()
         {
             return String.Format("Id Cliente: {0} - Nome Cliente: {1} - Nif Cliente: {2} - Morada Cliente: {3} - Telemovel Cliente: {4}", idCliente.ToString(), nomeCliente, nifCliente.ToString(), moradaCliente, telemovelCliente.ToString());
         }
 
+        /// <summary>
+        /// Função que verifica se o objeto fornecido é igual ao Cliente.
+        /// </summary>
         public override bool Equals(object obj)
         {
             if (obj is Cliente)
